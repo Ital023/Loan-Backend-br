f71ce1780aec02b9feb35bb7c76cea7e078d79bc
Ital023 <contadoitalo911@gmail.com>
2024-07-13 22:37:53 -0300

    feat: logica do emprestimo pessoal

--- src/main/java/io/github/ital023/loans/domain/Customer.java
+++ src/main/java/io/github/ital023/loans/domain/Customer.java
@@ -2,4 +2,35 @@ package io.github.ital023.loans.domain;
 
 public class Customer {
 
+    private Integer age;
+    private String cpf;
+    private String nome;
+    private Double income;
+    private String location;
+
+    public Customer(Integer age, String cpf, String nome, Double income, String location) {
+        this.age = age;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.income = income;
+        this.location = location;
+    }
+
+
+    public boolean IsIncomeEqualOrLowerThan(double value) {
+        return income <= value;
+    }
+
+    public boolean isIncomeBetween(double minValue, double maxValue){
+        return income >= minValue && income <= maxValue;
+    }
+
+    public boolean isAgeLowerThan(int value) {
+        return age < value;
+    }
+
+    public boolean isFromLocation(String value) {
+        return location.equalsIgnoreCase(value);
+    }
+
 }
--- src/main/java/io/github/ital023/loans/domain/Loan.java
+++ src/main/java/io/github/ital023/loans/domain/Loan.java
@@ -1,4 +1,31 @@
 package io.github.ital023.loans.domain;
 
 public class Loan {
+
+    private Customer customer;
+
+    public Loan(Customer customer) {
+        this.customer = customer;
+    }
+
+    public boolean isPersonalLoanAvailable(){
+        if(customer.IsIncomeEqualOrLowerThan(3000.0)){
+            return true;
+        }
+
+        return customer.isIncomeBetween(3000.00,5000.00)
+                && customer.isAgeLowerThan(30)
+                && customer.isFromLocation("SP");
+
+    }
+
+    public double getPersonalLoanInterestedRate(){
+        if(isPersonalLoanAvailable()){
+            return 4.0;
+        }
+
+        throw new LoanNotAvailableException();
+
+    }
+
 }
