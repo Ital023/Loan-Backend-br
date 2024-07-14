b31f7a8417b4b2b96f044d89003fd8a47e8039db
Ital023 <contadoitalo911@gmail.com>
2024-07-13 23:46:57 -0300

    feat: estruturando o Response Loan

--- src/main/java/io/github/ital023/loans/controller/dto/LoanRequestDTO.java
+++ src/main/java/io/github/ital023/loans/controller/dto/LoanRequestDTO.java
@@ -1,4 +1,11 @@
 package io.github.ital023.loans.controller.dto;
 
-public record LoanRequestDTO(Integer age, String cpf, String nome, Double income, String location) {
+import io.github.ital023.loans.domain.Customer;
+
+public record LoanRequestDTO(Integer age, String cpf, String name, Double income, String location) {
+
+    public Customer toCustomer(){
+        return new Customer(age,cpf,name,income,location);
+    }
+
 }
--- src/main/java/io/github/ital023/loans/service/LoanService.java
+++ src/main/java/io/github/ital023/loans/service/LoanService.java
@@ -1,16 +1,30 @@
 package io.github.ital023.loans.service;
 
+import io.github.ital023.loans.controller.dto.LoanDTO;
 import io.github.ital023.loans.controller.dto.LoanRequestDTO;
 import io.github.ital023.loans.controller.dto.LoanResponseDTO;
+import io.github.ital023.loans.domain.Customer;
+import io.github.ital023.loans.domain.Loan;
+import io.github.ital023.loans.domain.enums.LoanType;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
 
     public LoanResponseDTO loanAvaibles(LoanRequestDTO loanRequestDTO){
+        Customer customer = loanRequestDTO.toCustomer();
+        Loan loan = new Loan(customer);
 
+        List<LoanDTO> loans = new ArrayList<>();
 
+        if(loan.isPersonalLoanAvailable()){
+            loans.add(new LoanDTO(LoanType.PERSONAL,loan.getPersonalLoanInterestedRate()));
+        }
 
+        return new LoanResponseDTO(loanRequestDTO.name(), loans);
     }
 
 
